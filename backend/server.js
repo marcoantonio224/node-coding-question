b158b72dedc38978c9e15e35c8b2e5c9b982bc12
Marco Canchola <marcocanchola2020@gmail.com>
2020-11-05 21:14:46 -0800

    Updated server with database connection to postgres/sequelize

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+// Custom modules for application
+const database = require('./config/database');
+// const routes = require('./routes');
+
+// The application's port number
+const PORT = process.env.PORT || 8000;
+
+// Run 'dotenv' to render application's environmental variables
+require('dotenv').config();
+
+// Render body parser for json objects
+app.use(bodyParser.json());
+// Render body parser for HTML forms
+app.use(bodyParser.urlencoded({ extended: true}));
+
+// Connect to the database
+database.create_connection();
+
+// Run application on PORT
+app.listen(PORT, ()=> console.log(`Divercity application is on port ${PORT} To terminate application, press 'ctrl + c' `));
