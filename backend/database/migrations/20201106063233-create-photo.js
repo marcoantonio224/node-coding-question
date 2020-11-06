53765f0a86fde042ccb11fa2b0931105e18a4315
Marco Canchola <marcocanchola2020@gmail.com>
2020-11-05 22:35:50 -0800

    Created user/photo models and migration files

---
+++ backend/database/migrations/20201106063233-create-photo.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Photos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      author: {
+        type: Sequelize.INTEGER,
+        allowNull:false,
+        references: {
+          model: 'User',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      photo: {
+        type: Sequelize.BLOB
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Photos');
+  }
+};
